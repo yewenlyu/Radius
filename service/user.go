568527f3d04b2cec3b97b9a95c0a3605729f5a91
yewenlyu2019 <yewenlyu@gmail.com>
2019-09-23 11:14:26 +0000

    second build: create user.go to handle signup and login

---
+++ service/user.go
@@ -0,0 +1,18 @@
+package main
+
+package main
+
+import (
+	    "context"
+		    "encoding/json"
+			    "errors"
+				    "fmt"
+					    "net/http"
+						    "reflect"
+							    "regexp"
+								    "time"
+
+									    jwt "github.com/dgrijalva/jwt-go"
+										    "github.com/olivere/elastic"
+										)
+
