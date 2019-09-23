0bc1b29e5e8f658ad4286a65c6692010f03b110a
yewenlyu2019 <yewenlyu@gmail.com>
2019-09-23 11:17:22 +0000

    second build: update user.go

--- service/user.go
+++ service/user.go
@@ -1,18 +1,29 @@
 package main
 
-package main
-
 import (
-	    "context"
-		    "encoding/json"
-			    "errors"
-				    "fmt"
-					    "net/http"
-						    "reflect"
-							    "regexp"
-								    "time"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+	"regexp"
+	"time"
 
-									    jwt "github.com/dgrijalva/jwt-go"
-										    "github.com/olivere/elastic"
-										)
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/olivere/elastic"
+)
 
+const (
+	USER_INDEX = "user"
+	USER_TYPE  = "user"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Age      int64  `json:"age"`
+	Gender   string `json:"gender"`
+}
+
+var mySigningKey = []byte("secret")
