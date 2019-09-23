4f2194c253fb6734e69de2ab9a1211811d1cf54b
yewenlyu2019 <yewenlyu@gmail.com>
2019-09-23 11:18:28 +0000

    second build: update user.go - addUser, checkUser

--- service/user.go
+++ service/user.go
@@ -27,3 +27,70 @@ type User struct {
 }
 
 var mySigningKey = []byte("secret")
+
+func checkUser(username, password string) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
+
+	query := elastic.NewTermQuery("username", username)
+
+	searchResult, err := client.Search().
+		Index(USER_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	var utyp User
+	for _, item := range searchResult.Each(reflect.TypeOf(utyp)) {
+		if u, ok := item.(User); ok {
+			if username == u.Username && password == u.Password {
+				fmt.Printf("Login as %s\n", username)
+				return nil
+			}
+		}
+	}
+
+	return errors.New("Wrong username or password")
+}
+
+func addUser(user User) error {
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
+	if err != nil {
+		return err
+	}
+
+	// select * from users where username = ?
+	query := elastic.NewTermQuery("username", user.Username)
+
+	searchResult, err := client.Search().
+		Index(USER_INDEX).
+		Query(query).
+		Pretty(true).
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	if searchResult.TotalHits() > 0 {
+		return errors.New("User already exists")
+	}
+
+	_, err = client.Index().
+		Index(USER_INDEX).
+		Type(USER_TYPE).
+		Id(user.Username).
+		BodyJson(user).
+		Refresh("wait_for").
+		Do(context.Background())
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User is added: %s\n", user.Username)
+	return nil
+}
