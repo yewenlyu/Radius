8db6879473f21c2926ff091336d9d26606eec84b
yewenlyu2019 <yewenlyu@gmail.com>
2019-09-23 12:09:56 +0000

    second build: add an annotate method to provide face annotation

--- service/ml.go
+++ service/ml.go
@@ -37,9 +37,76 @@ type MLRequestBody struct {
 }
 
 const (
-	// Replace this project ID and model name with your configuration.
-	PROJECT = "around-179500"
+	PROJECT = "radius-252718"
 	MODEL   = "face_face"
 	URL     = "https://ml.googleapis.com/v1/projects/" + PROJECT + "/models/" + MODEL + ":predict"
 	SCOPE   = "https://www.googleapis.com/auth/cloud-platform"
 )
+
+/**
+ * This function annotates a image file based on ml model, return score and error if exists.
+ */
+func annotate(r io.Reader) (float64, error) {
+	buf, err := ioutil.ReadAll(r)
+	if err != nil {
+		fmt.Printf("Cannot read image data %v\n", err)
+		return 0.0, err
+	}
+
+	client, err := google.DefaultClient(context.Background(), SCOPE)
+	if err != nil {
+		fmt.Printf("Failed to create HTTP client %v\n", err)
+		return 0.0, err
+	}
+
+	// Construct a ML request
+	requestBody := &MLRequestBody{
+		Instances: []Instance{
+			{
+				ImageBytes: ImageBytes{
+					B64: buf,
+				},
+				Key: "1",
+			},
+		},
+	}
+	jsonRequestBody, err := json.Marshal(requestBody)
+	if err != nil {
+		fmt.Printf("Failed to create ML request body %v\n", err)
+		return 0.0, err
+	}
+
+	request, err := http.NewRequest("POST", URL, strings.NewReader(string(jsonRequestBody)))
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Printf("Failed to send ML request %v\n", err)
+		return 0.0, err
+	}
+
+	jsonResponseBody, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Failed to get ML response body %v\n", err)
+		return 0.0, err
+	}
+
+	if len(jsonResponseBody) == 0 {
+		fmt.Println("Empty prediction response body")
+		return 0.0, errors.New("Empty prediction response body")
+	}
+
+	var responseBody MLResponseBody
+	if err := json.Unmarshal(jsonResponseBody, &responseBody); err != nil {
+		fmt.Printf("Failed to decode ML response %v\n", err)
+		return 0.0, err
+	}
+
+	if len(responseBody.Predictions) == 0 {
+		fmt.Println("Empty prediction result")
+		return 0.0, errors.New("Empty prediction result")
+	}
+
+	results := responseBody.Predictions[0]
+	fmt.Printf("Received a prediction result %f\n", results.Scores[0])
+	return results.Scores[0], nil
+}
